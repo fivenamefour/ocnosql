767e388c852b60a96baf6e12684133a2ce0398f0
fivenamefour <fivenamefour@gmail.com>
2013-01-23 16:36:51 +0800

    add drop-down menu.

--- ocnosql-web/src/main/webapp/js/main.js
+++ ocnosql-web/src/main/webapp/js/main.js
@@ -3,22 +3,58 @@ $(function() {
     var treeData = {}, tableList = viewList = [];
 
     var setting = {
-        view : { showLine : false },
-        data: {
-            simpleData: { enable: true }
+        view : {
+            showLine : false,
+            addHoverDom: addHoverDom,
+            removeHoverDom: removeHoverDom
+        },
+        edit: {
+            enable: true,
+            showRemoveBtn : false,
+            showRenameBtn : false
         }
     };
+
+    function addHoverDom(treeId, treeNode) {
+        var node = $("#" + treeNode.tId + "_a");
+        var dropdown;
+
+        if (treeNode.level === 0) return;  // If it is the root node, there is no drop-down menu
+        if ($("div[id$=_" + treeNode.tId + "]").length > 0)  return; // If there is a drop-down menu, return
+
+        if (treeNode.type === "tableRoot") {
+            dropdown = $.template($('#tplCreateTable').html(), {tId : treeNode.tId});
+        } else if (treeNode.type === "table") {
+            dropdown = $.template($('#tplOperateTable').html(), {tId : treeNode.tId});
+        } else if (treeNode.type === "viewRoot") {
+           // todo
+        } else if (treeNode.type === "view") {
+           // todo
+        }
+
+        dropdown && node.append(dropdown);
+
+        $("a[op]").click(function() {
+            alert($(this).attr("op"));
+        });
+    }
+
+    function removeHoverDom(treeId, treeNode) {
+        $("div[id$=_" + treeNode.tId + "]").remove();
+        return false;
+    }
+
     // $.when($.ajax1, $.ajax2, ...).then(doneCallbacks, failCallbacks)
     $.when($.rest("GET", "${restPath}/table/list.json", function(data) {
         for (var i = 0, l = data.length; i < l; i++) {
-            tableList.push({"id": data[i].tableId, "name": data[i].tableName});
+            tableList.push({"id": data[i].tableId, "name": data[i].tableName, "type": "table"});
         }
     }), $.rest("GET", "${restPath}/view/list.json", function(data) {
         for (var i = 0, l = data.length; i < l; i++) {
-            viewList.push({"id": data[i].viewId, "name": data[i].viewName});
+            viewList.push({"id": data[i].viewId, "name": data[i].viewName, "type": "view"});
         }
     })).then(function() {
-        treeData = {name: "OcNoSql集群一", children: [{name: "OcNoSql表", children: tableList}, {name: "视图", children: viewList}]};
+        treeData = {name: "OcNoSql集群一", children: [{name: "OcNoSql表", type: "tableRoot", children: tableList}, {name: "视图", type: "viewRoot", children: viewList}]};
         $.fn.zTree.init($("#clusterTree"), setting, treeData);
     })
 });
--- ocnosql-web/src/main/webapp/main.jsp
+++ ocnosql-web/src/main/webapp/main.jsp
@@ -4,14 +4,47 @@
 <head>
   <meta charset="UTF-8">
   <title>OcNoSql管理</title>
+  <link rel="stylesheet" href="${frontendPath}/lib/bootstrap/bootstrap/css/bootstrap.min.css" type="text/css"/>
+  <link rel="stylesheet" href="${frontendPath}/extend/bootstrap/bootstrap/css/bootstrap.css" type="text/css"/>
   <link rel="stylesheet" href="${frontendPath}/lib/ztree/JQuery zTree v3.4/css/zTreeStyle/zTreeStyle.css" type="text/css"/>
-  <link rel="stylesheet" href="${frontendPath}/extend/ztree/ztree/css/zTreeStyle/zTreeStyle.css" type="text/css">
+  <link rel="stylesheet" href="${frontendPath}/extend/ztree/ztree/css/zTreeStyle/zTreeStyle.css" type="text/css"/>
 
 </head>
 
 <body>
-<ul id="clusterTree" class="ztree"></ul>            
+<ul id="clusterTree" class="ztree"></ul>
+
+<!-- 下拉操作模板：新建表 -->
+<div id="tplCreateTable" style="display:none;">
+  <div class="dropdown" id="menuCreateTable_{tId}">
+    <a class="dropdown-toggle icon icon-dropdown" data-toggle="dropdown"></a>
+    <ul class="dropdown-menu">
+      <li><a href="#" op="create">新建表</a></li>
+    </ul>
+  </div>
+</div>
+
+<!-- 下拉操作模板：表操作 -->
+<div id="tplOperateTable" style="display:none;">
+  <div class="dropdown" id="menuOperateTable_{tId}">
+    <a class="dropdown-toggle icon icon-dropdown" data-toggle="dropdown"></a>
+    <ul class="dropdown-menu">
+      <li><a href="#" op="disable">预删除</a></li>
+      <li><a href="#" op="delete">删除</a></li>
+      <li><a href="#" op="rename">重命名</a></li>
+      <li><a href="#" op="copy">复制</a></li>
+      <li><a href="#" op="query">查询</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+
 <script type="text/javascript" src="${frontendPath}/lib/jquery/jquery/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="${frontendPath}/lib/bootstrap/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="${frontendPath}/extend/bootstrap/bootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="${frontendPath}/lib/ztree/JQuery zTree v3.4/js/jquery.ztree.all-3.4.min.js"></script>
 <script type="text/javascript" src="${frontendPath}/core/osfc.js"></script>
 <script type="text/javascript" src="${frontendPath}/lib/json/json/json2.js"></script>
